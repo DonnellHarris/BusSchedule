f27115e347be9821967688f6537bd4ab1b2cf88a
Donnell Harris <142928416+DonnellHarris@users.noreply.github.com>
2023-11-09 23:38:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/busschedule/BusScheduleApp.kt
@@ -0,0 +1,8 @@
+package com.example.busschedule
+
+import android.app.Application
+import com.example.busschedule.data.BusScheduleDatabase
+
+class BusScheduleApp: Application() {
+    val database: BusScheduleDatabase by lazy { BusScheduleDatabase.getDatabase(this) }
+}
---
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.busschedule.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Database class with a singleton Instance object.
+ */
+@Database(entities = [BusSchedule::class], version = 1, exportSchema = false)
+abstract class BusScheduleDatabase: RoomDatabase() {
+
+    abstract fun busScheduleDao(): BusScheduleDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: BusScheduleDatabase? = null
+
+        fun getDatabase(context: Context): BusScheduleDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(context, BusScheduleDatabase::class.java, "bus_schedule")
+                    .createFromAsset("database/bus_schedule.db")
+                    // Wipes and rebuilds instead of migrating if no Migration object.
+                    // Migration is not part of this codelab.
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
+}
